0058d1de66e620f0d6162f862f9eb047335f9e3d
Taylor Birkeland <birkelandt@hed.local>
2018-02-15 23:36:29 -0800

    Updating production ENV

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,7 +1,23 @@
 import { Environment } from './environment.interface';
 
+const getWsUrl = ():string => {
+	let wsProtocol = 'ws:';
+	let host = window.location.host;
+	let httpProtocol = window.location.protocol;
+	if (httpProtocol == 'https:') {
+		wsProtocol = 'wss:';
+	}
+	return `${wsProtocol}//${host}/`;
+};
+
+const getApiUrl = ():string => {
+	let host = window.location.host;
+	let httpProtocol = window.location.protocol;
+	return `${httpProtocol}//${host}/api/`;
+};
+
 export const ENV: Environment = {
 	DEBUG : false,
-	WS_URL : 'ws://192.168.5.1/',
-	API_URL : 'http://192.168.5.1/api/'
+	WS_URL : getWsUrl(),
+	API_URL : getApiUrl()
 };
